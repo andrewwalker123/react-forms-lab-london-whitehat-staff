9c1025a525767139989cbb20c0b0121654a8bbb9
andrew.walker123 <andrew.walker3@sky.uk>
2019-03-26 14:20:16 +0000

    Done

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,24 +4,39 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
-
+  onChangeHandler = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+  onSubmitHandler = (event) => {
+    event.preventDefault()
+    if (this.state.username !== '' && this.state.password !== '') {
+    return this.props.onSubmit(this.state)
+    } else {
+      return
+    }
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmitHandler}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" value={this.state.username} type="text" onChange={this.onChangeHandler}/>
           </label>
         </div>
+
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" value={this.state.password} type="password" onChange={this.onChangeHandler}/>
           </label>
         </div>
+
         <div>
           <button type="submit">Log in</button>
         </div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,21 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
   }
-
+  onChangeHandler = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+  calculateRemainingChars = () => this.props.maxChars - this.state.message.length
+  
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" name='message' value={this.state.message} onChange={this.onChangeHandler}/>
+        <p>{this.calculateRemainingChars()}</p>
       </div>
     );
   }
